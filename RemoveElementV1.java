06babeaa91bcdda0a1a9fe2a4bc31ad933d58c3e
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-29 18:39:21 -0500

    Created a RemoveElementV1 class - working code

---
+++ RemoveElementV1.java
@@ -0,0 +1,15 @@
+/*
+ * Author: Nick Burkett
+ * Date: 12/29/24
+ * 
+ * Problem: Remove instances of a value from a given array. 
+ */
+
+//The class that will drive this algo
+public class RemoveElementV1{
+        
+        //The main method for the class
+        public static void main(String[] args){
+                System.out.println("ha, yea right");
+        }
+}
