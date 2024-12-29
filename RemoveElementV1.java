c216744b5e85b7abd0d48e220c1b52f1ca75ed72
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-29 18:48:52 -0500

    Added removeElement() template

--- RemoveElementV1.java
+++ RemoveElementV1.java
@@ -8,6 +8,18 @@
 //The class that will drive this algo
 public class RemoveElementV1{
         
+        /*
+         * removeElement function - This will remove any instances of a given value and return...
+         * ... the number of elements in the array post removal of given value. 
+         *
+         * @param nums[int] - Array of integers. 
+         * @param int val - Value that needs to be removed from the array.
+         * @return int count - Number of elements left in the array post edit.
+         */
+        public int removeElement(int[] nums, int val){
+                
+        }
+
         //The main method for the class
         public static void main(String[] args){
                 System.out.println("ha, yea right");
