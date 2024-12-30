6c76f9714682eff223b294262fb67ee988fbab55
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-29 20:28:02 -0500

    Solution tested - Passed

--- RemoveElementV1.java
+++ RemoveElementV1.java
@@ -21,42 +21,68 @@ public class RemoveElementV1{
                 //Delcare/initialize counter var to hold value of non val integers in the array. 
                 int counter = 0; 
 
-                int i = 0;  //Initialize counter variable for loop below. 
-                int lenOfNums = nums.length;    //Save the lenth of nums[] that's passed in. 
-                
+                int i = 0;                      //Initialize counter variable for loop below. 
+                int lenOfNums = nums.length;    //Save the lenth of nums[] that's passed in.
+
                 //Create loop to iterate through array in search of values equal to val. 
                 while(i < lenOfNums){
                         
                         //Need to check if I have a correct match where nums[i] == val is T. 
                         if(nums[i] == val){
-                                
+
+                                counter += 1;   //Var to keep track of number of elemets flag. 
+
                                 //Need to create a helper variable to keep track of internal placement. 
                                 int j = i; 
 
                                 //Create a inner loop that will reorder array to remove the valued integer. 
-                                while(j < lenOfNums){
+                                while(j < (lenOfNums-1)){
                                         nums[j] = nums[j+1];    //Bump up each element. 
-                                        j ++
+                                        j += 1;
                                 }
-
-                                //Check if this is the first iteration of the loop.
-                                if(j == i){
-                                        nums[lenOfNums-1] = "_";
+                                nums[lenOfNums-1] = (-1);   //Replace the last element with "-1".
+                                
+                                //Now I need to check if we have the integer that I bumped to the front is !=  to val. 
+                                if(nums[i] != val){
+                                        i += 1;             //Increment i by one.
                                 }
                         }
-
-                        //Now I need to check if we have the integer that I bumped to the front is !=  to val. 
-                        if(nums[i] != val){
-                                i += 1;             //Increment i by one.
-                                counter += 1;       //Increment counter by one
+                        else{
+                                i += 1;     //Increment i by 1. 
                         }
                 }
                 
-                return counter;     //Return the number of elements in nums that did != val. 
+                return lenOfNums - counter;     //Return the number of elements in nums that did != val. 
         }
 
         //The main method for the class
         public static void main(String[] args){
-                System.out.println("ha, yea right");
+                
+                //Need to create some data to test with
+                int[] arrT1 = {1, 2, 3, 3, 3, 4, 5}; 
+                int varT1 = 9;      //Integer that I want to remove. 
+                
+                RemoveElementV1 tmpObj = new RemoveElementV1();    //Create a new RemoveElementV1 object. 
+                
+                System.out.println(tmpObj.removeElement(arrT1, varT1));
+
+                
+
         }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
